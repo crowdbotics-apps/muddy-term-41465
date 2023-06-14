9b51f1dc76448c6d11e0e556aa2e2d0ac08eb644
Crowdbotics <team@crowdbotics.com>
2023-06-14 09:39:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-term-41465",
+  url: "https://muddy-term-41465.botics.co",
+  api: "https://muddy-term-41465.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
